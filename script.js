00d7179d9ab68d298eb250112d9138b5ea24957a
mahunn <maahin.ahmaad@gmail.com>
2025-08-28 18:35:22 +0600

    added the logic in js for cards section, added cards styles

---
+++ script.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const services = [    {
+            icon: 'assets/emergency.png',
+            name: 'National Emergency Number',
+            englishName: 'National Emergency',
+            number: '999',
+            category: 'All'
+        },
+     {
+            icon: 'assets/police.png',
+            name: 'Police Helpline Number',
+            englishName: 'Police',
+            number: '999',
+            category: 'Police'
+        },
+        
+     
+       
+        {
+            icon: 'assets/fire-service.png',
+            name: 'Fire Service Number',
+            englishName: 'Fire Service',
+            number: '999',
+            category: 'Fire'
+        },
+        {
+            icon: 'assets/ambulance.png',
+            name: 'Ambulance Service',
+            englishName: 'Ambulance',
+            number: '1994-999999',
+            category: 'Health'
+        },
+        {
+            icon: 'assets/emergency.png',
+            name: 'Women & Child Helpline',
+            englishName: 'Women & Child Helpline',
+            number: '109',
+            category: 'Help'
+        },
+        {
+            icon: 'assets/emergency.png',
+            name: 'Anti-Corruption Helpline',
+            englishName: 'Anti-Corruption',
+            number: '106',
+            category: 'Govt'
+        },
+        {
+            icon: 'assets/emergency.png',
+            name: 'Electricity Helpline',
+            englishName: 'Electricity Outage',
+            number: '16216',
+            category: 'Electricity'
+        },
+        {
+            icon: 'assets/emergency.png',
+            name: 'Brac Helpline',
+            englishName: 'Brac',
+            number: '16445',
+            category: 'NGO'
+        },
+        {
+            icon: 'assets/emergency.png',
+            name: 'Bangladesh Railway Helpline',
+            englishName: 'Bangladesh Railway',
+            number: '163',
+            category: 'Travel'
+        }
+    ];
+
+
+    
+    });
